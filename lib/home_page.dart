cf6b3aa56b906591293288a2392f92082a4b6320
Cedrick Pablo <cedrick.pablo71@gmail.com>
2023-03-22 16:29:15 +0800

    feat: implemented async action > - Implemented pokemon actions - Added pokemon list on app state model - Removed comments and testing code on main.dart - Created a string constant file for the get pokemon list action parameters

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:pokedex_start/state/action/pokemon_actions.dart';
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    StoreProvider.dispatch(context, GetPokemonsAction());
+
     return const MaterialApp(
       home: Scaffold(
         body: Center(
--- lib/state/app_state.dart
+++ lib/state/app_state.dart
@@ -1,3 +1,4 @@
+import 'package:pokedex_start/api/model/pokemon.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'app_state.freezed.dart';
@@ -8,6 +9,7 @@ class AppState with _$AppState {
   factory AppState({
     // TODO: This one will be remove
     @Default('') String sample,
+    @Default(<Pokemon>[]) List<Pokemon> pokemons,
   }) = _AppState;
 
   factory AppState.fromJson(Map<String, dynamic> json) => _$AppStateFromJson(json);
